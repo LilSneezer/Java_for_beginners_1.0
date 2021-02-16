b87661a5210779a9a8863d0c7a0b36ea6c16e734
Andrew <lilsneezer87@gmail.com>
2021-02-17 00:39:08 +0300

    Cycles. Task 9

---
+++ IdeaProjects/Курс Java для начинающих 1.0/src/Cycle9.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Cycle9 {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int arrLength = scanner.nextInt();
+        double[] arr = new double[arrLength];
+        double sum = 0;
+        for (int i = 0; i < arrLength; i++) {
+            arr[i] = scanner.nextDouble();
+            sum += arr[i];
+        }
+        double avg = sum / arrLength;
+        for (int i = 0; i < arrLength; i++) {
+            arr[i] *= avg;
+        }
+        String doubleArrString = Arrays.toString(arr);
+        System.out.println(doubleArrString);
+    }
+}
