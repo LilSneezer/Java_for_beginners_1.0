d12b70edf6fa8ecbeb2c4412f8506dfbf0caff39
Andrew <lilsneezer87@gmail.com>
2021-02-17 00:05:44 +0300

    Cycles. Task 8

---
+++ IdeaProjects/Курс Java для начинающих 1.0/src/Cycle8.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Cycle8 {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите любое положительное число");
+        int n = scanner.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i += 2) {
+            sum += i;
+        }
+        System.out.println("Сумма нечетных чисел равна: " + sum);
+    }
+}
