5d62a5f3bd1194c8d5f043f6f580f7921e68492a
Andrew <lilsneezer87@gmail.com>
2021-03-07 11:55:55 +0300

    output fix

--- IdeaProjects/Курс Java для начинающих 1.0/src/Cycle10.java
+++ IdeaProjects/Курс Java для начинающих 1.0/src/Cycle10.java
@@ -6,19 +6,17 @@ public class Cycle10 {
         System.out.println("Введите размерность матрицы: ");
         int a = scanner.nextInt();
         int b = scanner.nextInt();
-        int[][] arr = new int[a][b];
+        double[][] arr = new double[a][b];
         System.out.println("Введите значения матрицы: ");
         for (int i = 0; i < a; i++) {
             for (int j = 0; j < b; j++) {
                 arr[i][j] = scanner.nextInt();
             }
         }
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                arr[0][j] = arr[0][j] * 3;
-                System.out.print(arr[i][j]);
-            }
-            System.out.println();
+        for (int j = 0; j < arr[0].length; j++) {
+            arr[0][j] = arr[0][j] * 3;
+            System.out.print(arr[0][j] + " ");
         }
+        System.out.println();
     }
 }
